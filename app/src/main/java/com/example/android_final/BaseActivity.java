2e523917b186f5c7e79c25c8aefafb69626bb6bd
PhoneWaiYanMoe <wyan40653@gmail.com>
2024-12-04 00:10:53 +0700

    IntroActivity before going to MainActivity

---
+++ app/src/main/java/com/example/android_final/BaseActivity.java
@@ -0,0 +1,27 @@
+package com.example.android_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.android_final.databinding.ActivityIntroBinding;
+
+public class BaseActivity extends AppCompatActivity {
+    ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window w= getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+    }
+}

4b89bd48780698850ecad117645086514e7aecfd
PhoneWaiYanMoe <wyan40653@gmail.com>
2024-12-05 14:15:15 +0700

    designing the user interface of the main page.

--- app/src/main/java/com/example/android_final/BaseActivity.java
+++ app/src/main/java/com/example/android_final/BaseActivity.java
@@ -2,23 +2,21 @@ package com.example.android_final;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.android_final.databinding.ActivityIntroBinding;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class BaseActivity extends AppCompatActivity {
-    ActivityIntroBinding binding;
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
         Window w= getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
